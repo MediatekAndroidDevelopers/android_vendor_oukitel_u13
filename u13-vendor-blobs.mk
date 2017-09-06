1ca13795a9d702ed3f35358fd8a5233a0efac846
fire855 <thefire855@gmail.com>
2017-09-06 22:26:42 +0200

    Move ramdisk services to vendor

--- u13-vendor-blobs.mk
+++ u13-vendor-blobs.mk
@@ -8,6 +8,4 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/u13/etc,sys
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/u13/usr,system/usr)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/u13/xbin,system/xbin)
-
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/u13/vendor,system/vendor)
