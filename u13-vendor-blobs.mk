608c3d7d75d4fe0762a8de0eca016c3430030ed8
fire855 <thefire855@gmail.com>
2017-11-25 15:09:54 +0100

    Move everything to vendor

--- u13-vendor-blobs.mk
+++ u13-vendor-blobs.mk
@@ -1,11 +1,3 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/u13/lib,system/lib)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/u13/lib64,system/lib64)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/u13/bin,system/bin)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/u13/etc,system/etc)
-
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/u13/usr,system/usr)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/u13/vendor,system/vendor)
